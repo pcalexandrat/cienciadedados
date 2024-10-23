b66ca73d88c8348d2a48d3e96e435f813906613b
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-10-23 19:38:45 -0300

    dados

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,5 +4,14 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que  do total de ${dados.total_pessoas_mundo} de pessoas no mundo,  
+             ${total_trabalhadores_mundo} são trabalhadores e aproximadamente ${dados.total_pessoas_que_necessitam_transporte_para_trabalho} necessitam de transporte
+            para ir ao trabalho?  E o tempo médio gasto para o deslocamento ao trabalho
+            é de menos de ${dados.tempo_medio_deslocamento_para_trabalho}`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+
 }
 vizualizarInformacoesGlobais()
