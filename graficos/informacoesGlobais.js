22e94020bbb7605fca60a8b95fe57e1f9bbce439
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-11-06 17:47:01 -0300

    dados.

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,7 +7,7 @@ async function vizualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que  do total de <span> ${dados.total_pessoas_mundo} </span> de pessoas no mundo,  
-            <span> ${total_trabalhadores_mundo} </span> são trabalhadores e aproximadamente <span> ${dados.total_pessoas_que_necessitam_transporte_para_trabalho} </span> necessitam de transporte
+            <span> ${dados.total_trabalhadores_mundo} </span> são trabalhadores e aproximadamente <span> ${dados.total_pessoas_que_necessitam_transporte_para_trabalho} </span> necessitam de transporte
             para ir ao trabalho?  E o tempo médio gasto para o deslocamento ao trabalho
             é de menos de <span> ${dados.tempo_medio_deslocamento_para_trabalho}</span>`
     const container = document.getElementById('graficos-container')
